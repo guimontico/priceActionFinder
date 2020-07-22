65fb0c3876cd0faa94811b522d1897f181b51be9
Guilherme Montico <38353832+guimontico@users.noreply.github.com>
2020-07-21 23:04:03 -0300

    Update projeto_stockalytics.py
    fixing index bug

--- projeto_stockalytics.py
+++ projeto_stockalytics.py
@@ -6,9 +6,6 @@ from alpha_vantage.timeseries import TimeSeries
 import pandas as pd
 from pandas import DataFrame
 
-# from pandas_datareader import data as web
-# import plotly.graph_objects as go
-
 #set the key and the output format 
 ALPHA_VANTAGE_API_KEY = 'FAMIMUBSM0W9GBRE'
 ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format='pandas')
@@ -21,10 +18,21 @@ intraday_data = intraday_data.sort_values(by='date', ascending=True)
 
 intraday_data.columns = ['Open', 'High', 'Low', 'Close', 'volume']
 
+intraday_data = intraday_data.reset_index()
+
 EURUSD_60min = pd.read_csv('data/eurusd.csv')
 
-EURUSD_60min = EURUSD_60min.append(intraday_data)
+EURUSD_60min = EURUSD_60min.drop(['Unnamed: 0'], axis=1)
+
+EURUSD_60min = EURUSD_60min.append(intraday_data, ignore_index=True)
 
 EURUSD_60min = EURUSD_60min.drop_duplicates()
+
+EURUSD_60min = EURUSD_60min.drop_duplicates(subset=['Open', 'High', 'Low', 'Close'], keep='first')
+
+for i, row in EURUSD_60min.iterrows():
+    EURUSD_60min.at[i,'date'] = pd.Timestamp(row.date)
+
+EURUSD_60min = EURUSD_60min.sort_values(by='date', ascending=True)
 
 EURUSD_60min.to_csv('data/eurusd.csv', index=False)
